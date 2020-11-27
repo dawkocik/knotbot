a17f5bd4f680108462b2a823050e12bda79a871a
dawkocik <dawkocik@gmail.com>
2020-11-27 21:03:07 +0100

    Get mentions function update

--- bot/util/other.py
+++ bot/util/other.py
@@ -22,12 +22,35 @@ def get_mentions(ctx: Context, count: int = 0) -> Union[List[User], User, None]:
             except ValueError:
                 pass
 
-            got_a_user = False
+            got_user = False
             for member in ctx.guild.members:
-                if arg.lower() == member.name.lower():
+                if member.name.lower().startswith(arg.lower()):
                     users.append(member)
-                    got_a_user = True
+                    got_user = True
                     break
 
-            if not got_a_user:
+            if not got_user:
                 return None
+    else:
+        if ctx.message.mentions is not None and ctx.message.mentions >= count:
+            return ctx.message.mentions[:range]
+
+        for arg in count:
+            try:
+                user = ctx.bot.get_user(int(ctx.args[arg]))
+                if user is not None:
+                    users.append(user)
+            except ValueError:
+                pass
+
+            got_user = False
+            for member in ctx.guild.members:
+                if member.name.lower().startswith(ctx.args[arg].lower):
+                    users.append(member)
+                    got_user = True
+                    break
+
+            if not got_user:
+                return None
+
+    return users if users else None
